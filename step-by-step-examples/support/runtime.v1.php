5ea4de09019b886849552a6e42ed6169f02ea830
Oleg Parashchenko <olpa@uucode.com>
2010-12-20 13:49:19 +0100

    1) Function "*" added 2) More tracing data printed.

--- step-by-step-examples/support/runtime.v1.php
+++ step-by-step-examples/support/runtime.v1.php
@@ -3,9 +3,9 @@
 define('DEBUG', TRUE);
 
 function glo_exit() {
-  global $fp, $stack;
+  global $fp, $reg1, $stack;
   if (DEBUG) {
-    print "\n\nProgram ended, fp=$fp, stack=$stack\n";
+    print "\n\nProgram ended, reg1=$reg1, fp=$fp, stack=$stack\n";
   }
   exit();
 }
@@ -38,14 +38,20 @@ function glo_x2d() { // -
   $pc = $reg0;
 }
 
+function glo_x2a() { // *
+  global $reg0, $reg1, $reg2, $pc;
+  $reg1 = $reg1 * $reg2;
+  $pc = $reg0;
+}
+
 function exec_scheme_code($pc_main) {
-  global $pc, $reg0, $stack, $fp;
+  global $pc, $reg0, $reg1, $reg2, $stack, $fp;
   $stack = array();
   $fp    = 0;
   $reg0  = 'glo_exit';
   $pc    = $pc_main;
   while (1) { // at some moment, glo_exit performs exit()
-    //print "pc='$pc'\n";
+    //print "pc='$pc', fp='$fp', reg1='$reg1', reg2='$reg2'\n";flush();
     $pc();
   }
 }
